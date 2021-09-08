849c2d89240e15d8f2db9382671bf64aace62997
Vortex1003 <49214847+Vortex1003@users.noreply.github.com>
2021-09-07 21:06:37 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
+do
+    local success, script = pcall(function()
+        return game:HttpGet("https://raw.githubusercontent.com/Vortex1003/Vortex-Hub/games/main/" .. game.GameId .. ".lua")
+    end)
 
+    if (success) then
+        loadstring(script)()
+    end
+end
